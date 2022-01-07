ab4bad6f536c773e4120e9a3054c72af9b4da6b4
herbwood <herbwood27@gmail.com>
2022-01-07 21:48:59 +0900

    added wandb name argparse

--- classification/test.sh
+++ classification/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-EXP_NAME=exp1
+EXP_NAME=naive_cam
 
 CUDA_VISIBLE_DEVICES=0  python3 ./scripts/test.py \
     --img_dir=../../dataset/VOC2012/JPEGImages/ \
@@ -10,4 +10,4 @@ CUDA_VISIBLE_DEVICES=0  python3 ./scripts/test.py \
     --input_size=256 \
 	  --num_classes=20 \
     --restore_from=./runs/${EXP_NAME}/model/pascal_voc_epoch_14.pth \
-    --save_dir=./runs/${EXP_NAME}/attention/ \
+    --save_dir=./runs/${EXP_NAME}/attention/ 
--- classification/train.sh
+++ classification/train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-EXP_NAME=naive_cam 
+EXP_NAME=naive_cam_visualization
 
-CUDA_VISIBLE_DEVICES=0 python3 ./scripts/train.py \
+CUDA_VISIBLE_DEVICES=1 python3 ./scripts/train.py \
     --img_dir=../../dataset/VOC2012/JPEGImages/ \
     --train_list=./data/train_cls.txt \
     --test_list=./data/val_cls.txt \
@@ -15,4 +15,5 @@ CUDA_VISIBLE_DEVICES=0 python3 ./scripts/train.py \
 	  --num_workers=8 \
 	  --snapshot_dir=./runs/${EXP_NAME}/model/  \
     --att_dir=./runs/${EXP_NAME}/accu_att/ \
-    --decay_points='5,10'
+    --decay_points='5,10' \
+    --wandb_name=${EXP_NAME}
